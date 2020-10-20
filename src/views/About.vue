4bcb7bffa9f7bf2566611ea38c594c6895b97175
AdrianGomes1 <adriangomes054@gmail.com>
2020-10-21 02:18:23 +0400

    added routing and routed buttons about and home, some visual bugs in the divs that need to be fixed

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,12 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <body>
+      <div class="buttons">
+          <button id='Home' @click="$router.push({'path' : '/'})">Home</button>
+          <button id='About' @click="$router.push('about')">About</button>
+          <button id='Contacts'>Contacts</button>
+          <button id='Works'>Works</button>
+      </div>
+    </body>
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,29 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <body>
+      <div class='topbar'>
+        <div class="buttons">
+          <button id='Home' @click="$router.push({'path' : '/'})">Home</button>
+          <button id='About' @click="$router.push('about')">About</button>
+          <button id='Contacts'>Contacts</button>
+          <button id='Works'>Works</button>
+        </div>
+        <p id='LocalTime'></p>
+      </div>
+      <div class='typedText'>
+        <h1 id='heading'>Welcome to my Page!</h1>
+      </div>
+      <p id='Desc'>
+      This is a page that includes all of my works as a developer such as websites, games, or apps
+      the github link of each work will be attached to it
+      </p>
+    </body>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
 }
 </script>
